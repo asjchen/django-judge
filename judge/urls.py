bd252eb122ed908a97ea8e60121f3ff53c9a3efa
asjchen <asjchen@users.noreply.github.com>
2015-11-04 23:26:28 -0800

    Update urls.py
    Added comments to URLs

--- judge/urls.py
+++ judge/urls.py
@@ -6,6 +6,7 @@ from . import views
 urlpatterns = [
 	url(r'^$', views.IndexView.as_view(), name = 'index'),
 
+# The following few views are specifically for account handling
 	url(r'^account/login/$', "django.contrib.auth.views.login", name = 'login'),
 	url(r'^account/logout/$', "django.contrib.auth.views.logout", {"next_page": "/judge/"}, name = 'logout'),
 	url(r'^account/password_change/$', "django.contrib.auth.views.password_change", {"post_change_redirect": "/judge/"}, name = 'password_change'),
@@ -19,6 +20,8 @@ urlpatterns = [
 	url(r'^problems/(?P<problem_slug>([^/]*))/send/$', login_required(views.get_entry, login_url="judge/account/login/"), name = 'send'),
 	url(r'^problems/(?P<slug>([^/]*))/leaderboard/$', views.LeaderboardView.as_view(), name = 'leaderboard'),
 	url(r'^problems/(?P<problem_slug>([^/]*))/leaderboard/(?P<entry_id>([^/]*))/$', views.entry_leaderboard, name = 'entry_leaderboard'),
+	
+# Upon submission of a problem, the client sees these pages in order
 	url(r'^problems/(?P<problem_slug>([^/]*))/(?P<source_key>([^/]*))/$', views.process_entry, name = 'process'),
 	url(r'^problems/(?P<problem_slug>([^/]*))/(?P<source_key>([^/]*))/compare/$', views.compare_answer, name = 'compare'),
 	url(r'^problems/(?P<problem_slug>([^/]*))/(?P<source_key>([^/]*))/to-results/$', views.to_results, name = 'to_results'),
